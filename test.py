5f033b53fcd711f1d2cb42d64e578155041d90bb
koh <kohiry@mail.ru>
2020-05-03 20:09:08 +0400

    реализация пароля для юза команд

--- test.py
+++ test.py
@@ -6,32 +6,45 @@ import random
 import random_roles
 
 
-TOKEN = "TOKEN"
+TOKEN = "NzA0NzQ4NTg2NjgzOTkwMjk2.Xq7CvQ.6JNSN1e6NC40hmEf55AO-p6FJhI"
 bot = commands.Bot(command_prefix='!')
+key = '_**.**_'
 
 
-@bot.command(name='help_u')
-async def help(self):
-    await self.send('Я есть творение Koh(a). Буду помогать вам.')
+def check(key_drop):
+    if key != key_drop:
+        print('код не подошёл')
+        return False
+    else:
+        print('код подошёл')
+        return True
+
+@bot.command(name='helpU')
+async def help(self, key):
+    if check(key):
+        print(self.message)
+        await self.send('Я есть творение Koh(a). Буду помогать вам.')
 
 
 @bot.command(name='rules')
-async def rules(self):
-    text = ''
-    with open('rules.txt') as f:
-        text = ' '.join(f.readlines())
-    await self.send(text)
+async def rules(self, key):
+    if check(key):
+        text = ''
+        with open('rules.txt') as f:
+            text = ' '.join(f.readlines())
+        await self.send(text)
 
 
 @bot.command(name='rename')
-async def renames(self):
-    count = 1
-    for member in bot.guilds[0].members:  # bot.guilds[1] - Mafia party сервер, пока не знаю как определить гильдию отправителя
-        try:
-            await member.edit(nick=str(count)) # or do whatever you wish with the member detail
-            count += 1
-        except discord.errors.Forbidden:
-            print('Нет прав на переименование, скипаю')
+async def renames(self, key):
+    if check(key):
+        count = 1
+        for member in bot.guilds[0].members:  # bot.guilds[1] - Mafia party сервер, пока не знаю как определить гильдию отправителя
+            try:
+                await member.edit(nick=str(count)) # or do whatever you wish with the member detail
+                count += 1
+            except discord.errors.Forbidden:
+                print('Нет прав на переименование, скипаю')
 
     # await self.send('#')  # таким образом буду автоматически вызывать нужные мне команды
 
