33e12b5d3d40a3d5c8b6f1e68c94b6af47230bbf
koh <kohiry@mail.ru>
2020-05-06 14:20:27 +0400

    убрал ненужное, оставил стабильное в test.py

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 import discord
-import requests
 import pprint
 from discord.ext import commands
-import random
 import random_roles
+import requests
 
 
 TOKEN = "NzA0NzQ4NTg2NjgzOTkwMjk2.Xq7CvQ.6JNSN1e6NC40hmEf55AO-p6FJhI"
 bot = commands.Bot(command_prefix='!')
 key = '_**.**_'
 
+roles = random_roles.main()
+pprint.pprint(roles)
 
 def check(key_drop):
     if key != key_drop:
@@ -18,13 +19,6 @@ def check(key_drop):
     else:
         print('код подошёл')
         return True
-
-
-@bot.command(name='helpU')
-async def help(self, key):
-    if check(key):
-        print(self.message)
-        await self.send('Я есть творение Koh(a). Буду помогать вам.')
 
 
 @bot.command(name='rules')
@@ -39,33 +33,6 @@ async def rules(self, key):
         with open('rules_card.txt') as f:
             text = ' '.join(f.readlines())
         await self.send(text)
-
-
-@bot.command(name='rename')
-async def renames(self, key):
-    if check(key):
-        count = 1
-        for member in bot.guilds[0].members:  # bot.guilds[1] - Mafia party сервер, пока не знаю как определить гильдию отправителя
-            try:
-                await member.edit(nick=None) # or do whatever you wish with the member detail
-                count += 1
-            except discord.errors.Forbidden:
-                print('Нет прав на переименование, скипаю')
-
-    # await self.send('#')  # таким образом буду автоматически вызывать нужные мне команды
-
-
-@bot.command(name='move')
-async def move(self, key):
-    if check(key):
-        voice = discord.channel.VoiceChannel
-        voices_channels = []
-        for i in bot.guilds[0].channels:
-            if type(i) == voice:
-                voices_channels.append(i)
-        await bot.guilds[0].members[1].move_to(voices_channels[3])
-        #for i in bot.guilds[0].members:
-            #print(i)  #move_to()
 
 
 @bot.event
